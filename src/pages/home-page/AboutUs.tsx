526993a83bfc2e66dba4cfecc81476ed6ccaf417
Shir Rotem <shirr@moveo.co.il>
2023-11-08 22:42:43 +0200

    test

--- src/pages/home-page/AboutUs.tsx
+++ src/pages/home-page/AboutUs.tsx
@@ -8,7 +8,7 @@ const AboutUs = () =>{
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             In a lacus vel justo fermentum bibendum non 
             eu ipsum. Cras porta malesuada eros, eget blandit
-            turpis suscipit at.  Vestibulum sed massa in magna sodales porta.  Vivamus elit urna, 
+            turpis suscipit at.  Vestibulum sed massa in magna sodales porta. Vivamus elit urna, 
             dignissim a vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             In a lacus vel justo fermentum bibendum no eu ipsum. Cras porta malesuada eros.
             </p>
