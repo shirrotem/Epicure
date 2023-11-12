dc48a5036931bdb72f99dd16b13130e97788b902
Shir Rotem <shirr@moveo.co.il>
2023-11-12 12:12:47 +0200

    adding usecallback

--- src/shared/hooks/useMobileCheck.ts
+++ src/shared/hooks/useMobileCheck.ts
@@ -1,19 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const useMobileCheck = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 900);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 900);
-    };
+  const handleResize = useCallback(()=>{
+    setIsMobile(window.innerWidth <= 900);
 
+  }, []);
+
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [handleResize]);
 
   return isMobile;
 };
