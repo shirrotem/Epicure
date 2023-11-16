2a325fa0b084fb8968a689bf1f6a2e4dff79a8f4
Shir Rotem <shirr@moveo.co.il>
2023-11-16 11:57:23 +0200

    popup commit

--- src/pages/home-page/components/HomePage/HomePage.tsx
+++ src/pages/home-page/components/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import AboutUs from "../AboutUs/AboutUs";
 import Hero from "../Hero/Hero";
 import Carousel from "../Carousel/Carousel";
@@ -9,11 +9,31 @@ import IconMeaning from "../IconMeaning/IconMeaning";
 import { iconsArray } from "../../../../data/MockData";
 
 
+import Modal from "../../../../shared/components/Modal/Modal";
+import ModalPortal from "../../../../shared/components/Modal/ModalPortal";
 
 const HomePage = () => {
 
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+      setShowModal(!showModal);
+    };
+
     return (
         <>
+           <div>
+      <h1>React Modal Example</h1>
+      <button onClick={toggleModal}>Open Modal</button>
+      <ModalPortal>
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <h2>Modal Content</h2>
+            <p>This is a modal window</p>
+          </Modal>
+        )}
+      </ModalPortal>
+    </div>
         <Hero/>
         <Carousel type={data.restaurants} typeName="restaurants" title="POPULAR RESTAURANT IN EPICURE:"/>
         <Carousel type={data.dishes} typeName="dishes" title="SIGNATURE DISH OF:"/>
---
+++ src/shared/components/Modal/Modal.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './Modal.scss';
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {children}
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
